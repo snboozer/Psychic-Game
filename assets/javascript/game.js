ce07b73774af8b14d5a6e95846225da70aef7cfe
Shamera Boozer <snboozer@gmail.com>
2019-06-08 10:15:09 -0400

    attempts at adding the guess count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,20 +15,29 @@ var computerGuessElement = document.getElementById("Computer-guess");
 console.log(userGuessElement)
 var WinsElement = document.getElementById("wins")
 var LossesElement = document.getElementById("losses")
-
+var guessesSoFarElement = document.getElementById("Guesses-So-Far");
+ 
+for (i=1; i<=5; i++)
+{
+document.write(i)   
+}
 
 document.onkeypress = function(event) {
     var userGuess = event.key;
     console.log(userGuess)
     userGuessElement.innerText=userGuess
 
+
     var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
     console.log(computerChoice)
+    
 
     if(userGuess === computerChoice){
         wins++;
     document.getElementById("wins").innerText=wins
         console.log(wins)
+        guessesNum = 9;
+        guessChoice = [];
     }
     
     else {
@@ -36,5 +45,15 @@ document.onkeypress = function(event) {
         console.log(losses)
         document.getElementById("losses").innerText=losses
     }
+
+    
+
+    
+}
+
+function newFunction() {
+    for (var guesses = 10 - 1; x >= 0; x--)
+        console.log(x);
+    return guesses;
 }
 
